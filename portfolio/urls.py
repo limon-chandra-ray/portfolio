3dcf2ff2ca1645f3c7c3318fd61b4987b6a5a222
limon-chandra-ray <limonroy.19cse013@gmail.com>
2023-06-28 00:20:00 +0600

    'template-views-urls-add'

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 app_name = 'portfolio'
 urlpatterns = [
-    path('',views.home,name='home')
+    path('',views.home,name='home'),
+    path('skill',views.skill,name='skill'),
+    path('contact-us',views.contactus,name='contactus'),
+    path('study',views.study,name='study'),
+    path('exprience',views.exprience,name='exprience')
 ]
--- portfolio/views.py
+++ portfolio/views.py
@@ -3,3 +3,17 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request,'home.html')
+
+
+def skill(request):
+    return render(request,'skill.html')
+
+def contactus(request):
+    return render(request,'contactus.html')
+
+def study(request):
+    return render(request,'study.html')
+
+def exprience(request):
+    return render(request,'exprience.html')
+
